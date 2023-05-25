5774bd35449d5529824872ba1db5901715d1ea9f
Carol Sousa <105009508+Caroline-crs@users.noreply.github.com>
2023-05-25 20:09:26 -0300

    Entrega

---
+++ TestForDotNet/Cartoon.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TestForDotNet.Class.Cartoon;
+
+namespace TestForDotNet.Class;
+public class Cartoon
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public object prev { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Origin
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Character
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        //public Origin origin { get; set; }
+        //public Location location { get; set; }
+        public string image { get; set; }
+
+        //public List<Episode> episode { get; set; }
+        public string url { get; set; }
+        public DateTime? created { get; set; }
+    }
+
+    public class Root
+    {
+        public Info info { get; set; }
+        public List<Character> results { get; set; }
+    }
+}
+
+public class Episode
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string air_date { get; set; }
+    public string episode { get; set; }
+    public List<Character> characters { get; set; }
+    public string url { get; set; }
+    public DateTime created { get; set; }
+}
--- TestForDotNet/Program.cs
+++ TestForDotNet/Program.cs
@@ -1,25 +1,134 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using TestForDotNet.Class;
+using static TestForDotNet.Class.Cartoon;
+
+async Task<List<Character>> GetCharacterRickAndMorty()
+{
+    string url = "https://rickandmortyapi.com/api/character";
+    Root root = new Root();
+
+    try
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string body = await response.Content.ReadAsStringAsync();
+
+                root = JsonConvert.DeserializeObject<Root>(body);
+
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    return root.results;
+}
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("Character"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
 
-app.UseAuthorization();
+List<Character> characterList = new List<Character>();
 
-app.MapControllers();
+async Task GetAndStoreCharacters()
+{
+    List<Character> characters = await GetCharacterRickAndMorty();
+    characterList.AddRange(characters);
+}
 
+app.MapGet("/Characters", async (AppDbContext dbContext) =>
+{
+    if (characterList.Count == 0)
+        await GetAndStoreCharacters();
+
+    return characterList;
+});
+
+app.MapGet("/Character/{id}", async (int id, AppDbContext dbContext) =>
+{
+    Character characterSingle = characterList.SingleOrDefault(c => c.id == id);
+
+    if (characterSingle != null)
+        return Results.Ok(characterSingle);
+
+    else
+        return Results.NotFound();
+});
+
+app.MapPut("/Character/{id}", async (int id, Character updateCharacter, AppDbContext dbContext) =>
+{
+    Character character = await dbContext.Characters.FindAsync(id);
+
+    if (character != null)
+    { 
+        character.name = updateCharacter.name;
+        character.status = updateCharacter.status;
+        character.gender = updateCharacter.gender;
+        character.species = updateCharacter.species;
+
+        dbContext.Entry(character).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+    }
+
+    else
+        Results.NotFound();
+
+
+    return updateCharacter;
+});
+
+
+app.MapDelete("/Character/{id}", async (int id, AppDbContext dbContext) =>
+{
+    Character character = await dbContext.Characters.FindAsync(id);
+
+    if (character != null)
+        dbContext.Characters.Remove(character);
+
+    await dbContext.SaveChangesAsync();
+
+    return;
+});
+
+
+app.MapPost("/Character", async (Character character, AppDbContext dbContext) =>
+{
+    dbContext.Characters.Add(character);
+    await dbContext.SaveChangesAsync();
+
+    return character;
+});
+
+app.UseSwaggerUI();
 app.Run();
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<Character> Characters { get; set; }
+    //public DbSet<Episode> Episodes { get; set; }
+}
